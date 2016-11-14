b59d35f241303a4d736df53275446fff1e235e68
Luke <lukeaveil@yahoo.com>
2016-11-14 16:48:17 +0000

    refactors graph functions, seems to be working again now but again having some caching issues

--- js/graph.js
+++ js/graph.js
@@ -1,59 +1,35 @@
-function populationGraph() {
-  var chart = c3.generate({
-    bindto: '#chart',
-    data: {
-      x: 'Country',
-      url: '/data/data.csv',
-      hide: ['GDP/Capita', 'Life Expectancy'],
-      type: 'bar'
-    },
-    axis: {
-      x: {
-        type: 'category'
+var chart = c3.generate({
+  bindto: '#chart',
+  data: {
+    x: 'Country',
+    url: '/data/data.csv',
+    type: 'bar'
+  },
+  axis: {
+    x: {
+      type: 'category'
+    }
+  },
+  legend: {
+        show: false
       }
-    },
-    legend: {
-          show: false
-        }
+});
+
+function populationGraph() {
+  chart.load({
+    hide: ['GDP/Capita', 'Life Expectancy']
   });
+
 }
 
 function gdpGraph() {
-  var chart = c3.generate({
-    bindto: '#chart',
-    data: {
-      x: 'Country',
-      url: '/data/data.csv',
-      hide: ['Population', 'Life Expectancy'],
-      type: 'bar'
-    },
-    axis: {
-      x: {
-        type: 'category'
-      }
-    },
-    legend: {
-          show: false
-        }
+  chart.load({
+    hide: ['Population', 'Life Expectancy']
   });
 }
 
 function lifeGraph() {
-  var chart = c3.generate({
-    bindto: '#chart',
-    data: {
-      x: 'Country',
-      url: '/data/data.csv',
-      hide: ['GDP/Capita', 'Population'],
-      type: 'bar'
-    },
-    axis: {
-      x: {
-        type: 'category'
-      }
-    },
-    legend: {
-          show: false
-        }
+  chart.load({
+    hide: ['Population', 'GDP/Capita']
   });
 }
