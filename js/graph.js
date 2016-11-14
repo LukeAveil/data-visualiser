fb274ea99a46a20c3a62bb7f825290dca2a2216d
Luke <lukeaveil@yahoo.com>
2016-11-14 16:41:24 +0000

    adds graph generators and onclclick methods, having lots of problems with caching and c3

--- js/graph.js
+++ js/graph.js
@@ -1,16 +1,59 @@
-var chart = c3.generate({
-  bindto: '#chart',
-  data: {
-    x: 'Country',
-    url: '/data/data.csv',
-    type: 'bar'
-  },
-  axis: {
-    x: {
-      type: 'category'
-    }
-  },
-  legend: {
-        show: false
+function populationGraph() {
+  var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      x: 'Country',
+      url: '/data/data.csv',
+      hide: ['GDP/Capita', 'Life Expectancy'],
+      type: 'bar'
+    },
+    axis: {
+      x: {
+        type: 'category'
       }
-});
+    },
+    legend: {
+          show: false
+        }
+  });
+}
+
+function gdpGraph() {
+  var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      x: 'Country',
+      url: '/data/data.csv',
+      hide: ['Population', 'Life Expectancy'],
+      type: 'bar'
+    },
+    axis: {
+      x: {
+        type: 'category'
+      }
+    },
+    legend: {
+          show: false
+        }
+  });
+}
+
+function lifeGraph() {
+  var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      x: 'Country',
+      url: '/data/data.csv',
+      hide: ['GDP/Capita', 'Population'],
+      type: 'bar'
+    },
+    axis: {
+      x: {
+        type: 'category'
+      }
+    },
+    legend: {
+          show: false
+        }
+  });
+}
