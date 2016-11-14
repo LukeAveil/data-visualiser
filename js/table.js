147938d6a2957cc064d27d0c04a90e8f5bc14cec
Luke <lukeaveil@yahoo.com>
2016-11-14 15:27:56 +0000

    adds setup for radio buttons, adds function and onclick event to show table and buttons

--- js/table.js
+++ js/table.js
@@ -1,15 +1,20 @@
-d3.text("data/data.csv", function(data) {
-  var parsedCSV = d3.csv.parseRows(data);
+function loadData() {
+  d3.text("data/data.csv", function(data) {
+    var parsedCSV = d3.csv.parseRows(data);
 
-  var container = d3.select("#table")
-    .append("table")
+    var container = d3.select("#table")
+      .append("table")
 
-    .selectAll("tr")
-      .data(parsedCSV).enter()
-      .append("tr")
+      .selectAll("tr")
+        .data(parsedCSV).enter()
+        .append("tr")
 
-    .selectAll("td")
-      .data(function(d) { return d; }).enter()
-      .append("td")
-      .text(function(d) { return d; });
-});
+      .selectAll("td")
+        .data(function(d) { return d; }).enter()
+        .append("td")
+        .text(function(d) { return d; });
+  });
+
+  document.getElementById("data").style.visibility = "visible";
+  document.getElementById("button").style.visibility = "hidden";
+}
